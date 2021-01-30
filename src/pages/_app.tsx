e12c0c78127fbdf17a30c5e07a6b12a38fa2d8fb
Yuri Oliveira <yurioliveira@aluno.fapce.edu.br>
2021-01-29 21:12:05 -0300

    style: Change font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,12 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta name="description" content="some text here" />
         <link rel="manifest" href="/manifest.json" />
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
