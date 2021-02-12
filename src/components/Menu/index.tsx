3f6aa8de6c9941e1bb84a9161122f6af1eaa5e77
Yuri Oliveira <yurioliveira@aluno.fapce.edu.br>
2021-02-11 21:47:58 -0300

    style: Add Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,29 +1,88 @@
 import { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
-import { Wrapper, IconWrapper, MenuFull } from './styles'
+import {
+  Wrapper,
+  IconWrapper,
+  MenuFull,
+  MenuNav,
+  MenuLink,
+  RegisterBox,
+  CreateAccount,
+  LogoWrapper
+} from './styles'
 import Logo from 'components/Logo'
+import Button from 'components/Button'
+import MediaMatch from 'components/MediaMatch'
 
-const Menu: React.FC = () => {
+export type MenuProps = {
+  username?: string
+}
+
+const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <Wrapper>
-      <IconWrapper onClick={() => setIsOpen(true)}>
-        <MenuIcon aria-label="open menu" />
-      </IconWrapper>
+      <MediaMatch lessThan="medium">
+        <IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="open menu" />
+        </IconWrapper>
+      </MediaMatch>
 
-      <Logo hideOnMobile />
-      <div>
+      <LogoWrapper>
+        <Logo hideOnMobile />
+      </LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <MenuNav>
+          <MenuLink href="#">Home</MenuLink>
+          <MenuLink href="#">Explore</MenuLink>
+        </MenuNav>
+      </MediaMatch>
+
+      <div className="menu-group">
         <IconWrapper>
           <SearchIcon aria-label="search" />
         </IconWrapper>
+        <IconWrapper>
+          <ShoppingCartIcon aria-label="shopping cart" />
+        </IconWrapper>
+
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sign In</Button>
+          </MediaMatch>
+        )}
       </div>
 
       <MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+
+        <MenuNav>
+          <MenuLink href="#">Home</MenuLink>
+          <MenuLink href="#">Explore</MenuLink>
+          {!!username && (
+            <>
+              <MenuLink href="#">My account</MenuLink>
+              <MenuLink href="#">Wishlist</MenuLink>
+            </>
+          )}
+        </MenuNav>
+        {!username && (
+          <RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </CreateAccount>
+          </RegisterBox>
+        )}
       </MenuFull>
     </Wrapper>
   )
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.menu`
   ${({ theme }) => css`
@@ -7,8 +8,26 @@ export const Wrapper = styled.menu`
     padding: ${theme.spacings.small} 0;
     justify-content: space-between;
   `}
-`
 
+  .menu-group {
+    ${({ theme }) => css`
+      display: flex;
+      flex-grow: 1;
+      justify-content: flex-end;
+      align-items: center;
+      > div {
+        margin-left: ${theme.spacings.xsmall};
+      }
+    `}
+  }
+`
+export const LogoWrapper = styled.div`
+  ${media.lessThan('medium')`
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  `}
+`
 export const IconWrapper = styled.div`
   ${({ theme }) => css`
     color: ${theme.colors.white};
@@ -17,12 +36,115 @@ export const IconWrapper = styled.div`
     height: 2.4rem;
   `}
 `
+export const MenuNav = styled.div`
+  ${({ theme }) => css`
+    ${media.greaterThan('medium')`
+			margin-left: ${theme.spacings.small};
+		`}
+  `}
+`
+
+export const MenuLink = styled.a`
+  ${({ theme }) => css`
+    position: relative;
+    color: ${theme.colors.white};
+    font-size: ${theme.font.sizes.medium};
+    margin: 0.3rem ${theme.spacings.small} 0;
+    text-decoration: none;
+    text-align: center;
+
+    &:hover {
+      &::after {
+        content: '';
+        position: absolute;
+        display: block;
+        height: 0.3rem;
+        background-color: ${theme.colors.primary};
+        animation: hoverAnimation 0.3s forwards;
+      }
+
+      @keyframes hoverAnimation {
+        from {
+          width: 0;
+          left: 50%;
+        }
+        to {
+          width: 100%;
+          left: 0;
+        }
+      }
+    }
+  `}
+`
+
 type MenuFullProps = {
   isOpen: boolean
 }
 
 export const MenuFull = styled.nav<MenuFullProps>`
-  ${({ isOpen }) => css`
+  ${({ isOpen, theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    background: ${theme.colors.white};
+    height: 100vh;
+    overflow: hidden;
     opacity: ${isOpen ? 1 : 0};
+    pointer-events: ${isOpen ? 'all' : 'none'};
+
+    > svg {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin: ${theme.spacings.xxsmall};
+      cursor: pointer;
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+
+    ${MenuNav} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex: 1;
+      flex-direction: column;
+    }
+
+    ${MenuLink} {
+      color: ${theme.colors.black};
+      font-weight: ${theme.font.bold};
+      font-size: ${theme.font.sizes.xlarge};
+      margin-bottom: ${theme.spacings.small};
+    }
+  `}
+`
+
+export const RegisterBox = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 ${theme.spacings.xlarge} ${theme.spacings.xlarge};
+
+    > span {
+      display: block;
+      margin: ${theme.spacings.xxsmall} 0;
+      font-size: ${theme.font.sizes.xsmall};
+    }
+  `}
+`
+
+export const CreateAccount = styled.a`
+  ${({ theme }) => css`
+    text-decoration: none;
+    color: ${theme.colors.primary};
+    border-bottom: 0.2rem solid ${theme.colors.primary};
   `}
 `
