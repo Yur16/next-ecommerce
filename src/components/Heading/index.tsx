fa268b6de795daf6b522bb942407c5079c37d466
Yuri Oliveira <yurioliveira@aluno.fapce.edu.br>
2021-02-15 20:26:36 -0300

    style: Add Footer

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,20 +1,32 @@
 import { Wrapper } from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary'
 }: HeadingProps) => {
   return (
-    <Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    <Wrapper
+      color={color}
+      lineLeft={lineLeft}
+      lineBottom={lineBottom}
+      lineColor={lineColor}
+      size={size}
+    >
       {children}
     </Wrapper>
   )
